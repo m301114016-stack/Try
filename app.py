9ad0fbbf33461e8216ee021723715ad1e245147f
m301114016-stack <m301114016@tmu.edu.tw>
2026-01-30 16:03:18 +0800

    Update app.py

--- app.py
+++ app.py
@@ -165,7 +165,3 @@ else:
     if st.button("🔄 重新填寫問卷"):
         st.session_state.clear()
         st.rerun()
-
-[connections.gsheets]
-spreadsheet = "https://docs.google.com/spreadsheets/d/1ObiBVelvCqCV9Gu0bPwGLBmQomOymAQ_wJVZO7aR7vI/edit?usp=sharing"
-  
